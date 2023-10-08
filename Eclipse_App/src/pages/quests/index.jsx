466109371ec7d33f620166b401d396c792be9f95
Alann Batista Cardoso Silva <alannsilva@prefeitura.sp.gov.br>
2023-10-08 14:49:38 -0300

    quests

--- Eclipse_App/src/pages/quests/index.jsx
+++ Eclipse_App/src/pages/quests/index.jsx
@@ -3,6 +3,12 @@ import { motion } from "framer-motion"
 import '../../style/css/quests.css';
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import RefreshIcon from '@mui/icons-material/Refresh';
+import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
 
 class Quests extends React.Component {
     constructor(props) {
@@ -10,21 +16,25 @@ class Quests extends React.Component {
         this.state = {
             stars: [],
             respostas_user: [],
+            resposta_quest: '',
             respostas: ['1'],
+            nota: 0,
+            send: true,
             quests: [
                 {
                     'pergunta': "Why do only some people on Earth see an eclipse at a given time?",
                     'posiveis_respostas': [
-                        "Not everyone on Earth sees an eclipse at a given time because the Moon's shadow on Earth is not very big, so only a small portion of places on Earth will see it.", 
-                        "Yes everyone on Earth sees an eclipse.", 
-                        "Yes everyone on Earth sees an eclipse at a given time because the Moon's shadow on Earth is very big, so only a small portion of places on Earth will see it.", 
-                        "Yes, Only a small portion of places on Earth will see eclipse, Moon's is very big.", 
-                        "Yes, Moon's is very big."],
+                        "Not everyone on Earth sees an eclipse at a given time because the Moon's shadow on Earth is not very big, so only a small portion of places on Earth will see it.",
+                        "Yes everyone on Earth sees an eclipse.",
+                        "Yes everyone on Earth sees an eclipse at a given time because the Moon's shadow on Earth is very big, everyone on Earth will see it.",
+                        "Yes, everyone on on Earth will see eclipse, Moon's is very big.",
+                        "Yes, the Moon's is very big."],
                 }
             ]
 
         }
         this.createStars = this.createStars.bind(this);
+        this.resposta_questao = this.resposta_questao.bind(this);
     }
 
     componentDidMount() {
@@ -58,18 +68,54 @@ class Quests extends React.Component {
         this.setState({ stars: stars_create })
     }
 
+    resposta_questao() {
+    
+        if (this.state.resposta_quest.length > 0) {
+        //this.setState({ respostas_user: [...this.state.respostas_user, this.state.resposta_quest] })
+        let nota = 0
+
+        this.state.respostas.map((item, index)=>{
+             if (item === this.state.resposta_quest){
+                nota++
+             }
+             
+         })  
+         nota = (nota/this.state.respostas.length) * 10
+         this.setState({nota: nota, send: true})
+            
+        }
+    }
+
+
 
 
     render() {
         return (
             <>
-                <div className='container_principal' style={{backgroundColor: "#000"}}>
-                {this.state.stars.map((item) => item)}
+                <div className='container_principal' style={{ backgroundColor: "#000" }}>
+
                     <div className='Quests-grid'>
-                        <div></div>
+                        
+                        <div> {this.state.stars.map((item) => item)}</div>
                         <div className='container_quests'>
                             <div></div>
                             <div className='card_quests'>
+                                {this.state.send?<>  
+                                            <div className='Nota'>
+                                                <h2>NOTA</h2>
+                                                <p>{this.state.nota}</p>
+                                            </div> 
+                                            <div className='rocket'>
+                                                <RocketLaunchIcon  sx={{ fontSize: '8rem'}}/>
+                                            </div> 
+                                            <div className='send_resp'>
+                                                <div>
+                                                    <Button variant="contained" onClick={() => this.setState({send: false})} endIcon={<RefreshIcon />}>
+                                                        
+                                                    </Button>
+                                                </div>
+                                            </div> 
+                                </>:<>
                                 {this.state.quests.map((item) => {
                                     return (
                                         <>
@@ -77,25 +123,40 @@ class Quests extends React.Component {
                                                 <p>{item.pergunta}</p>
                                             </div>
                                             <div className='respostas'>
-                                                {item.posiveis_respostas.map((item, index) => {
-                                                    return (
-                                                    <ol type='a'>
-                                                        <li> <span className='list_ordem'>{index+1}</span>.<input type="checkbox" id="scales" name="scales" /> {item}</li>
-                                                    </ol>
-                                                )
-                                                })}
+                                                <FormControl>
+                                                    <RadioGroup
+                                                        aria-labelledby="demo-radio-buttons-group-label"
+                                                        name="radio-buttons-group"
+                                                        value={this.state.resposta_quest}
+                                                        onChange={(e) => this.setState({ resposta_quest: e.target.value })}
+                                                    >
+                                                        {item.posiveis_respostas.map((item, index) => {
+                                                            return (
+                                                                <ol type='a'>
+                                                                    <li><FormControlLabel value={index+1} control={<Radio />} label={<> {item}</>} /></li>
+                                                                </ol>
+                                                            )
+                                                        })}
+                                                    </RadioGroup>
+                                                </FormControl>
 
                                             </div>
                                             <div className='send_resp'>
                                                 <div>
-                                                    <Button variant="contained" endIcon={<SendIcon />}>
-                                                      Send
+                                                    <Button variant="contained" disabled>
+                                                        Next
+                                                    </Button>
+                                                    &nbsp;
+                                                    <Button variant="contained" onClick={() => this.resposta_questao()} endIcon={<SendIcon />}>
+                                                        Send
                                                     </Button>
                                                 </div>
                                             </div>
                                         </>
                                     )
                                 })}
+                                </>}
+                                
                             </div>
                             <div></div>
                         </div>
